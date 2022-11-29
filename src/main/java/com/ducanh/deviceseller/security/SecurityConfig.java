c503c82b1d5262e00b6635862d56df5ae0d0b0dc
NguyenDucAnh <ducanhkhtn@gmail.com>
2022-11-29 12:36:40 +0700

    u[date

--- src/main/java/com/ducanh/deviceseller/security/SecurityConfig.java
+++ src/main/java/com/ducanh/deviceseller/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
+        http.cors().disable();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
